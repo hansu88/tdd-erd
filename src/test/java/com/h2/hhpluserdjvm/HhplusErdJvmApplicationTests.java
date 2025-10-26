1708b41526c55db04b11d96131e4b2c211b3a316
hansu88 <gimhans0917@gmail.com>
2025-10-26 12:59:15 +0900

    Initial commit

---
+++ src/test/java/com/h2/hhpluserdjvm/HhplusErdJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2.hhpluserdjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusErdJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
