526f47872fb6c663b601a921d3c20dffa60ed268
hansu88 <gimhans0917@gmail.com>
2025-10-30 18:16:30 +0900

    "chore: db 초기화 삭제 및 Repository 클래스 삭제"

--- src/main/java/com/h2/hhpluserdjvm/repository/GoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.h2.hhpluserdjvm.repository;
-
-import com.h2.hhpluserdjvm.entity.Goods;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GoodsRepository extends JpaRepository<Goods, Long> {
-}
