1f574799b91a7dc77326019d49ab019efef0ddb9
hansu88 <gimhans0917@gmail.com>
2025-10-30 09:05:09 +0900

    "feat: 상품 별 재고 확인(큰 항목별)"

--- src/main/java/com/h2/hhpluserdjvm/controller/ProductController.java
+++ src/main/java/com/h2/hhpluserdjvm/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.h2.hhpluserdjvm.controller;
 
 import com.h2.hhpluserdjvm.dto.ProductDto;
+import com.h2.hhpluserdjvm.dto.ProductOptionDto;
 import com.h2.hhpluserdjvm.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -14,9 +15,13 @@ public class ProductController {
         this.productService = productService;
     }
 
-    // 상품 목록 조회
     @GetMapping
     public List<ProductDto> getProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/{productId}/stock")
+    public int getProductStock(@PathVariable Long productId) {
+        return productService.getTotalStock(productId);
     }
 }
--- src/main/java/com/h2/hhpluserdjvm/service/ProductService.java
+++ src/main/java/com/h2/hhpluserdjvm/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.h2.hhpluserdjvm.service;
 
 import com.h2.hhpluserdjvm.dto.ProductDto;
+import com.h2.hhpluserdjvm.dto.ProductOptionDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
@@ -23,5 +24,24 @@ public class ProductService {
     public List<ProductDto> getAllProducts() {
         ProductDto[] products = restTemplate.getForObject(MOCK_API_BASE + "/products", ProductDto[].class);
         return Arrays.asList(products);
+    }
+
+    /*
+    Todo: 하나의 상품 재고 확인
+     */
+
+    public List<ProductOptionDto> getProductStock(Long productId) {
+        ProductOptionDto[] options = restTemplate.getForObject(
+                MOCK_API_BASE + "/productOptions?productId=" + productId,
+                ProductOptionDto[].class
+        );
+        return Arrays.asList(options);
+    }
+
+    public int getTotalStock(Long productId) {
+        List<ProductOptionDto> options = getProductStock(productId);
+        return options.stream()
+                .mapToInt(ProductOptionDto::getAvailableStock)
+                .sum();
     }
 }
